2fd4066415d89c6bf4b554572004140cfffcce5d
dltmdrl1244 <dltmdrl1244@naver.com>
2023-08-02 00:18:25 -0700

    Update: 모바일 UI 초기 화면

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Layout = () => {
+    return (
+        <div className="flex justify-center h-screen bg-gray-300">
+            <div className="w-425 border-2 bg-white border-black rounded-2xl my-8 p-10">
+                {/* <Navbar /> */}
+            </div>
+        </div>
+    );
+}
+
+export default Layout;
