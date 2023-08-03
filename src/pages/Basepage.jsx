3b6415e1f496ae77f535d6c4e9c35b04e1854742
dltmdrl1244 <dltmdrl1244@naver.com>
2023-08-02 20:39:26 -0700

    Update: base structure (Navbar, Layout)

---
+++ src/pages/Basepage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { BsBatteryFull, BsBarChartFill } from "react-icons/bs";
+import { AiOutlineWifi } from "react-icons/ai";
+import { Navbar } from "../components";
+
+const Basepage = () => {
+    return (
+        <>
+            1
+        </>
+    );
+}
+
+export default Basepage;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
+import { BsBatteryFull, BsBarChartFill } from "react-icons/bs";
+import { AiOutlineWifi } from "react-icons/ai";
+import { Navbar } from "../components";
+import {Basepage} from "../pages";
 
+const SystemBar = () => {
+    let today = new Date();
+    return (
+        <div className="flex justify-between h-10 p-1">
+            <div>
+                {today.getHours()}:{today.getMinutes()}
+            </div>
+
+            <div className="flex gap-2">
+                <BsBarChartFill />
+                <AiOutlineWifi />
+                <BsBatteryFull />
+            </div>
+        </div>
+    );
+}
 const Layout = () => {
     return (
-        <div className="flex justify-center h-screen bg-gray-300">
-            <div className="w-425 border-2 bg-white border-black rounded-2xl my-8 p-10">
-                {/* <Navbar /> */}
+        <div className="flex justify-center bg-gray-300 h-screen w-screen">
+            <div className="flex flex-col w-425 h-725 border-4 my-auto bg-white border-black rounded-2xl p-3 p-t-2">
+                <SystemBar />
+                <div className="flex flex-1 border-1 flex-col">
+                    <Basepage />
+                </div>
+                <Navbar />
             </div>
         </div>
     );
